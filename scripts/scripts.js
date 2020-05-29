d94e93929bd6223a364fdee29b0e109aa54ce9a6
will c <will.cra@outlook.com>
2020-05-28 21:27:44 -0400

    improved presentation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,17 +7,17 @@ numberButton.addEventListener("click", showNumbers);
 letterButton.addEventListener("click", showLetters);
 
 function dropdown(event) {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("DropMenu").classList.toggle("show");
 }
 
 function showNumbers(event) {
     document.getElementById("numbers").style.display = "block";
     document.getElementById("letters").style.display = "none";
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("DropMenu").classList.toggle("show");
 }
 
 function showLetters(event) {
     document.getElementById("numbers").style.display = "none";
     document.getElementById("letters").style.display = "block";
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("DropMenu").classList.toggle("show");
 }
